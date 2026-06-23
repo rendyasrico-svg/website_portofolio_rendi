0148825dbc9a21cae38e19f2a0208c906fb59636
rendyasrico-svg <rendyasrico@gmail.com>
2026-06-23 10:51:30 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,23 @@
-self.addEventListener("install", () => {
-  console.log("Portfolio PWA Installed");
+const CACHE_NAME = "rendi-portfolio-v1";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./script.js",
+        "./profile.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
